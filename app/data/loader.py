85102d9863b1ace25137605a1a8b3e31d0a114c3
TellezM <160159516+TellezM@users.noreply.github.com>
2025-12-05 01:18:51 -0600

    data funciona

--- app/data/loader.py
+++ app/data/loader.py
@@ -1,2 +1,7 @@
 import fastf1
 fastf1.Cache.enable_cache("app/data/cache")
+
+def load_race(year, circuit):
+    session = fastf1.get_session(year, circuit, "R")
+    session.load()
+    return session
